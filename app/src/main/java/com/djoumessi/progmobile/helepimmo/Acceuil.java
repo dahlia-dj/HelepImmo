964986820dc9cf563d486fdaf519bbad6f7294e3
dahlia-dj <dahliadjoumessi18@gmail.com>
2022-03-22 14:32:31 +0100

    Initial Commit

---
+++ app/src/main/java/com/djoumessi/progmobile/helepimmo/Acceuil.java
@@ -0,0 +1,14 @@
+package com.djoumessi.progmobile.helepimmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Acceuil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
---
+++ app/src/main/java/com/djoumessi/progmobile/helepimmo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.djoumessi.progmobile.helepimmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.djoumessi.progmobile.helepimmo.ui.login.Login;
+
+public class MainActivity extends AppCompatActivity {
+     private Button connexion , register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        connexion = findViewById(R.id.connexion);
+        register = findViewById(R.id.register);
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent connex = new Intent(MainActivity.this, Login.class);
+                startActivity(connex);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(MainActivity.this, Formulaire.class);
+                startActivity(register);
+            }
+        });
+    }
+}
