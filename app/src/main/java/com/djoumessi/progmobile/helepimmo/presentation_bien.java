423e0b2a6f515fb929d42745627ac7e7e738d795
dahlia-dj <dahliadjoumessi18@gmail.com>
2022-03-25 16:43:09 +0100

    Presentation d'un bien

---
+++ app/src/main/java/com/djoumessi/progmobile/helepimmo/presentation_bien.java
@@ -0,0 +1,14 @@
+package com.djoumessi.progmobile.helepimmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class presentation_bien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentation_bien);
+    }
+}
