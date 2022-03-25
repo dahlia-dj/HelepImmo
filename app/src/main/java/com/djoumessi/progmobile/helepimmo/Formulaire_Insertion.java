50044ee3c6efa77552459f2e1295cbf29a4cadbd
dahlia-dj <dahliadjoumessi18@gmail.com>
2022-03-25 11:34:58 +0100

    voici le formulaire d'insertion d'un bien

---
+++ app/src/main/java/com/djoumessi/progmobile/helepimmo/Formulaire_Insertion.java
@@ -0,0 +1,14 @@
+package com.djoumessi.progmobile.helepimmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulaire_Insertion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulaire_insertion);
+    }
+}
